836f4c827a38fa092e2129ba16bfcd03cc55e0bc
Nabilahatamip <nabilahatamiputri888@gmail.com>
2026-06-10 06:17:12 +0700

    add Dockerfile and update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,22 @@
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sora_Backend'
-})
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'sora_backend',
+    waitForConnections: true,
+    connectionLimit: 10,
+});
 
-// Test koneksi
 db.getConnection()
   .then(conn => {
-    console.log('MySQL Connected')
-    conn.release()
+    console.log('MySQL Connected');
+    conn.release();
   })
   .catch(err => {
-    console.log('MySQL Error:', err.message)
-  })
+    console.log('MySQL Error:', err.message);
+  });
 
-module.exports = db
+module.exports = db;
