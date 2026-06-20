22155ca7ad375c3df8b47704f2912e5f0d8ec360
Nabilahatamip <nabilahatamiputri888@gmail.com>
2026-06-20 20:44:57 +0700

    add upload foto route

--- app.js
+++ app.js
@@ -70,3 +70,8 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+const fs = require('fs');
+
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
